345d0bed05968fda3eaafecbe9a4a94894e7eaf9
log101 <ffrknerdm@gmail.com>
2024-05-04 09:18:31 +0300

    chore: remove lit feat: add main header component

--- src/components/header.js
+++
@@ -1,9 +0,0 @@
-import { LitElement, html } from 'lit';
-
-export class Title extends LitElement {
-    render() {
-        return html`<h1>Log101</h1>`;
-    }
-}
-
-customElements.define('main-title', Title);
