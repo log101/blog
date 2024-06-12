f86697e00dadd6ed16bc0ec17b3cfa8668162eb5
log101 <ffrknerdm@gmail.com>
2024-06-12 15:10:14 +0300

    refactor: simplify code

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,6 +1,20 @@
-const cells = ['30', '20', '51', '01', '62', '63', '03', '64', '54', '44', '34', '24', '14']
+const cells = [
+  "30",
+  "20",
+  "51",
+  "01",
+  "62",
+  "63",
+  "03",
+  "64",
+  "54",
+  "44",
+  "34",
+  "24",
+  "14",
+];
 
 export const heavyWeightSpaceshipCell = (x, y) => {
-    const coor = `${x}${y}`
-    return cells.includes(coor)
-}
+  const coordinates = `${x}${y}`;
+  return cells.includes(coordinates);
+};
