393aa52757c4357ff265a1c1b873b3910a9ed4ee
log101 <ffrknerdm@gmail.com>
2024-06-21 12:53:17 +0300

    feat: add pagination to category page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.log101.dev",
+  redirects: {
+    "/category/[category]": "/category/[category]/1",
+  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
