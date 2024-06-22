82af925f4622e59d037aaf5bceae4262ddda9e8e
log101 <ffrknerdm@gmail.com>
2024-06-22 18:15:56 +0300

    feat: add header links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.log101.dev",
-  redirects: {
-    "/category/[category]": "/category/[category]/1",
-  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
